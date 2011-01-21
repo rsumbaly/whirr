7d8178b26e5cbeeccfa27a48dad177d49e2ad934
Roshan Sumbaly <rsumbaly@gmail.com>
2011-01-20 20:03:34 -0800

    First version of Voldemort service

---
+++ services/voldemort/src/main/java/org/apache/whirr/service/voldemort/VoldemortClusterActionHandler.java
@@ -0,0 +1,107 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.whirr.service.voldemort;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.whirr.service.Cluster;
+import org.apache.whirr.service.Cluster.Instance;
+import org.apache.whirr.service.ClusterActionEvent;
+import org.apache.whirr.service.ClusterActionHandlerSupport;
+import org.apache.whirr.service.ClusterSpec;
+import org.apache.whirr.service.ComputeServiceContextBuilder;
+import org.apache.whirr.service.jclouds.FirewallSettings;
+import org.jclouds.compute.ComputeServiceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VoldemortClusterActionHandler extends ClusterActionHandlerSupport {
+  
+  private static final Logger LOG =
+    LoggerFactory.getLogger(VoldemortClusterActionHandler.class);
+    
+  public static final String VOLDEMORT_ROLE = "voldemort";
+  public static final int CLIENT_PORT = 6666;
+  public static final int ADMIN_PORT = 6667;
+  public static final int HTTP_PORT = 8081;
+  public static final String newLineChar = System.getProperty("line.separator");
+  
+  public static final String PARTITIONS_PER_NODE = "whirr.voldemort.partitions";
+  
+  @Override
+  public String getRole() {
+    return VOLDEMORT_ROLE;
+  }
+  
+  @Override
+  protected void beforeBootstrap(ClusterActionEvent event) throws IOException {
+    addRunUrl(event, "sun/java/install");
+    addRunUrl(event, "linkedin/voldemort/install");
+  }
+
+  @Override
+  protected void beforeConfigure(ClusterActionEvent event)
+      throws IOException, InterruptedException {
+    ClusterSpec clusterSpec = event.getClusterSpec();
+    Cluster cluster = event.getCluster();
+    
+    LOG.info("Authorizing firewall");
+    ComputeServiceContext computeServiceContext =
+      ComputeServiceContextBuilder.build(clusterSpec);
+    FirewallSettings.authorizeIngress(computeServiceContext,
+        cluster.getInstances(), clusterSpec, CLIENT_PORT);
+    FirewallSettings.authorizeIngress(computeServiceContext,
+        cluster.getInstances(), clusterSpec, ADMIN_PORT);
+    FirewallSettings.authorizeIngress(computeServiceContext,
+            cluster.getInstances(), clusterSpec, HTTP_PORT);
+    
+    String servers = Joiner.on(newLineChar).join(getPublicIps(cluster.getInstances()));
+    
+    Configuration config = event.getClusterSpec().getConfiguration();
+    int partitionsPerNode = config.getInt(PARTITIONS_PER_NODE, 10);
+    addRunUrl(event, "linkedin/voldemort/post-configure",
+        "-c", clusterSpec.getProvider(), "-p", Integer.toString(partitionsPerNode), servers);
+  }
+
+  @Override
+  protected void afterConfigure(ClusterActionEvent event) {
+    ClusterSpec clusterSpec = event.getClusterSpec();
+    Cluster cluster = event.getCluster();
+    LOG.info("Completed setup of Voldemort {}", clusterSpec.getClusterName());
+    String hosts = Joiner.on(',').join(getPublicIps(cluster.getInstances()));
+    LOG.info("Hosts: {}", hosts);
+  }
+  
+  private List<String> getPublicIps(Set<Instance> set) {
+    return Lists.transform(Lists.newArrayList(set),
+        new Function<Instance, String>() {
+      @Override
+      public String apply(Instance instance) {
+        return instance.getPrivateAddress().getHostAddress();
+      }
+    });
+  }
+}
---
+++ services/voldemort/src/test/java/org/apache/whirr/service/voldemort/integration/VoldemortServiceTest.java
@@ -0,0 +1,113 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.whirr.service.voldemort.integration;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.apache.commons.configuration.CompositeConfiguration;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.whirr.service.Cluster;
+import org.apache.whirr.service.Cluster.Instance;
+import org.apache.whirr.service.voldemort.VoldemortClusterActionHandler;
+import org.apache.whirr.service.ClusterSpec;
+import org.apache.whirr.service.Service;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import voldemort.client.ClientConfig;
+import voldemort.client.SocketStoreClientFactory;
+import voldemort.client.StoreClient;
+import voldemort.client.protocol.admin.AdminClient;
+import voldemort.client.protocol.admin.AdminClientConfig;
+
+public class VoldemortServiceTest {
+
+  private final static int NUM_ATTEMPTS = 3;
+  private final static int NUM_KEYS = 1000;
+  
+  private ClusterSpec clusterSpec;
+  private Service service;
+  private Cluster cluster;
+
+  @Before
+  public void setUp() throws Exception {
+    CompositeConfiguration config = new CompositeConfiguration();
+    if (System.getProperty("config") != null) {
+      config.addConfiguration(new PropertiesConfiguration(System.getProperty("config")));
+    }
+    config.addConfiguration(new PropertiesConfiguration("whirr-voldemort-test.properties"));
+    clusterSpec = ClusterSpec.withTemporaryKeys(config);
+    
+    service = new Service();
+    cluster = service.launchCluster(clusterSpec);
+
+    waitForBootstrap();
+  }
+
+  private void waitForBootstrap() {
+    for ( Instance instance : cluster.getInstances()) {
+	  for (int attempts = 0 ; attempts < NUM_ATTEMPTS ; attempts++ ) {
+	    try {
+		  AdminClient client = new AdminClient("tcp://" + 
+		                                       instance.getPublicAddress() + 
+		                                       ":" + VoldemortClusterActionHandler.ADMIN_PORT, 
+		                                       new AdminClientConfig());
+		  client.getAdminClientCluster();
+	    } catch (Exception e) {
+	      System.out.print(".");
+	      try {
+	        Thread.sleep(1000);
+	      } catch (InterruptedException e1) {
+	        break;
+	      }
+	    }
+	  }
+    }
+  }
+    
+  @Test
+  public void testInstances() throws Exception {
+      Set<Instance> instances = cluster.getInstances();
+      Instance firstInstance = instances.iterator().next();
+      ClientConfig config = new ClientConfig().setBootstrapUrls( 
+                                "tcp://" + firstInstance.getPublicAddress() + 
+                                ":" + VoldemortClusterActionHandler.CLIENT_PORT );
+      SocketStoreClientFactory factory = new SocketStoreClientFactory( config );
+      StoreClient<String, String> client = factory.getStoreClient( "test" );
+      
+      for ( int keyId = 0; keyId < NUM_KEYS ; keyId++ ) {
+          client.put( "key" + keyId, "value" + keyId );
+      }
+      
+      for ( int keyId = 0; keyId < NUM_KEYS ; keyId ++ ) {
+          Assert.assertEquals( client.get( "key" + keyId ).getValue(), "value" + keyId );
+      }
+  }
+  
+  @After
+  public void tearDown() throws IOException, InterruptedException {
+    if (service != null) {
+      service.destroyCluster(clusterSpec);      
+    }
+  }
+
+}
